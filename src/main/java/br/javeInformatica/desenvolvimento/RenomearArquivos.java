5f70bb863d9aeecb41ba73d5b626a18624d79940
marcosvsilva <marcos.v.silva@live.com>
2016-07-13 09:59:35 -0300

    final commit

--- src/main/java/br/javeInformatica/desenvolvimento/RenomearArquivos.java
+++ src/main/java/br/javeInformatica/desenvolvimento/RenomearArquivos.java
@@ -8,15 +8,28 @@ import java.io.File;
 public class RenomearArquivos
 {
     private static String caminhoExecucao;
+    private static final String extensao = ".xml";
+    private static int inicioNumeroNota = 28;
+    private static int finalNumeroNota = inicioNumeroNota + 6;
 
     public static void main(String[] args)
     {
         File caminho = new File(".");
+        caminhoExecucao = caminho.getAbsolutePath();
         File arquivos[] = caminho.listFiles();
 
-        for (int i = 0; i < arquivos.length; i++)
+        try
         {
-            System.out.println(arquivos[i].getName());
-        }
+            for (File arquivo: arquivos)
+            {
+                String nameArquivo = arquivo.getName();
+                if (nameArquivo.substring(nameArquivo.indexOf('.'),nameArquivo.length()).equals(extensao))
+                {
+                    String nameNew = nameArquivo.substring(inicioNumeroNota,finalNumeroNota) + "nf" + extensao;
+                    File arquivoNew = new File(nameNew);
+                    arquivo.renameTo(arquivoNew);
+                }
+            }
+        } catch (StringIndexOutOfBoundsException e) {}
     }
 }
